3c6521adaacc83f04b317f3f5398c2e262350b78
Soorya003 <sooryasoor003@gmail.com>
2024-08-11 11:35:14 +0530

    updated

--- backend/routes/feedback.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendFeedback } = require('../controllers/feedbackController');
-
-router.post('/', sendFeedback);
-
-module.exports = router;
